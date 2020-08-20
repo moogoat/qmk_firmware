55dcc4cea87274c0adb158372733a22949cf1a38
Jay <jayzhang89@gmail.com>
2020-08-20 00:35:40 -0600

    Hopefully fixed the sticky-enter bug with some lessons learned from xd75re. Also shrunk the filesize by disabling some rules

--- keyboards/preonic/keymaps/moogoat/keymap.c
+++ keyboards/preonic/keymaps/moogoat/keymap.c
@@ -21,8 +21,6 @@
 
 enum preonic_layers {
   _QWERTY = 0,
-  _COLEMAK,
-  _DVORAK,
   _ARROWS,
   _RAISE,
   _ADJUST,
@@ -43,6 +41,8 @@ enum preonic_keycodes {
   MACRO_COMMA,
   MACRO_PERIOD
 };
+
+#define KC_SHEN MT(MOD_RSFT, KC_ENT)
 
 #define TD_START_KEYCODE 0x5700 // this is from the source code, quantum_keycodes.h
 #define MACRO_START 1
@@ -100,49 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,  \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    LT(_ARROWS, KC_J),    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT, KC_ENT),  \
-  KC_LCTL, KC_LGUI, TD(TD_NUMPAD_LOCK), KC_LALT, NUMPAD,   KC_SPC,  KC_SPC,  RAISE, KC_MINS, MO(_MACRO1), MO(_MACRO2), MO(_MACRO3)  \
-),
-
-/* Colemak
- * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   F  |   P  |   G  |   J  |   L  |   U  |   Y  |   ;  | \    |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   R  |   S  |   T  |   D  |   H  |N/LOW |   E  |   I  |   O  |  "   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Sh/Ent|
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | CTRL | GUI  |Numpad| ALT  |Numpad|    Space    |Raise |   -  |MACRO1|MACRO2|MACRO3|
- * `-----------------------------------------------------------------------------------'
- */
-[_COLEMAK] = LAYOUT_preonic_grid( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,  \
-  KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    LT(_ARROWS, KC_N),    KC_E,    KC_I,    KC_O,    KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT, KC_ENT),  \
-  KC_LCTL, KC_LGUI, TD(TD_NUMPAD_LOCK), KC_LALT, NUMPAD,   KC_SPC,  KC_SPC,  RAISE, KC_MINS, MO(_MACRO1), MO(_MACRO2), MO(_MACRO3)  \
-),
-
-/* Dvorak
- * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |   "  |   ,  |   .  |   P  |   Y  |   F  |   G  |   C  |   R  |   L  | \    |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   O  |   E  |   U  |   I  |   D  | H/Low|   T  |   N  |   S  |  /   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Sh/Ent|
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | CTRL | GUI  |Numpad| ALT  |Numpad|    Space    |Raise |   -  |MACRO1|MACRO2|MACRO3|
- * `-----------------------------------------------------------------------------------'
- */
-[_DVORAK] = LAYOUT_preonic_grid( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSLS,  \
-  KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    LT(_ARROWS, KC_H),    KC_T,    KC_N,    KC_S,    KC_SLSH, \
-  KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    MT(MOD_RSFT, KC_ENT),  \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SHEN,  \
   KC_LCTL, KC_LGUI, TD(TD_NUMPAD_LOCK), KC_LALT, NUMPAD,   KC_SPC,  KC_SPC,  RAISE, KC_MINS, MO(_MACRO1), MO(_MACRO2), MO(_MACRO3)  \
 ),
 
@@ -163,7 +121,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______,   _______, _______,  _______, _______, _______, _______, _______, _______, _______, \
   _______, KC_HOME, KC_BSPC, KC_UP, KC_DEL, KC_END, _______, _______, _______, _______, _______, KC_DEL,  \
   _______, C(KC_LEFT), KC_LEFT, KC_DOWN, KC_RIGHT, C(KC_RIGHT), _______, _______, _______, _______, _______, _______, \
-  KC_LSFT, _______, _______,   _______, _______,  _______, _______, _______, _______, _______, _______, MT(MOD_RSFT, KC_ENT), \
+  KC_LSFT, _______, _______,   _______, _______,  _______, _______, _______, _______, _______, _______, KC_SHEN, \
   KC_LCTL, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT \
 ),
 
@@ -184,7 +142,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, _______, KC_EQL, S(KC_EQL), \
   _______, KC_F9, KC_F10, KC_F11, KC_F12, _______, _______, _______, _______, _______, _______, KC_DEL,  \
   _______, _______, S(KC_COMM), S(KC_LBRC), S(KC_9), KC_LBRC, KC_RBRC, S(KC_0), S(KC_RBRC), S(KC_DOT), _______, _______, \
-  KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, MT(MOD_RSFT, KC_ENT), \
+  KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_SHEN, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
 ),
 
@@ -305,18 +263,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case QWERTY:
           if (record->event.pressed) {
             set_single_persistent_default_layer(_QWERTY);
-          }
-          return false;
-          break;
-        case COLEMAK:
-          if (record->event.pressed) {
-            set_single_persistent_default_layer(_COLEMAK);
-          }
-          return false;
-          break;
-        case DVORAK:
-          if (record->event.pressed) {
-            set_single_persistent_default_layer(_DVORAK);
           }
           return false;
           break;
@@ -504,3 +450,19 @@ qk_tap_dance_action_t tap_dance_actions[] = {
  [CT_EGG] = ACTION_TAP_DANCE_FN (dance_egg)
 };
 */
+
+bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_SHEN:
+            return true;
+        default: return false;
+    }
+}
+
+bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode ) {
+        case KC_SHEN:
+            return true;
+        default: return false;
+    }
+}
