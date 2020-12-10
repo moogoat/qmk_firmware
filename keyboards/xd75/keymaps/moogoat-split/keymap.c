807ee47f5b1284a25a4dc5f075a361b70387e3e0
Jay <jayzhang89@gmail.com>
2020-12-10 15:26:28 -0700

    Changed gaming layout a bit after playtesting

--- keyboards/xd75/keymaps/moogoat-split/keymap.c
+++ keyboards/xd75/keymaps/moogoat-split/keymap.c
@@ -272,22 +272,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * .--------------------------------------------------------------------------------------------------------------------------------------.
  * | `      | 1      | 2      | 3      | 4      | 5      | 6      | 7      | 8      | 9      | 0      | 0      | -      | =      | BACKSP |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * | TAB    | Q      | W      | E      | R      | T      | DM1    | -M4    | Volup  | Y      | U      | I      | O      | P      | \      |
+ * | TAB    | Q      | W      | E      | R      | T      | F1     | F2     | Volup  | Y      | U      | I      | O      | P      | \      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
- * | ESC    | A      | S      | D      | F      | G      | DM2    | -M5    | Mute   | H      | J      | K      | L      | ;      | '      |
+ * | ESC    | A      | S      | D      | F      | G      | F3     | F4     | Voldn  | H      | J      | K      | L      | ;      | '      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
- * | LSHIFT | Z      | X      | C      | V      | B      | -M3    | UP     | Voldn  | N      | M      | ,      | .      | /      | Enter  |
+ * | LSHIFT | Z      | X      | C      | V      | B      | DM1    | UP     | Mute   | N      | M      | ,      | .      | /      | Enter  |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * | LCTRL  | 2GUI   | TurTog | LALT   | SPACE           | LEFT   | DOWN   | RIGHT  | SPACE           | TurSpd | Exit            | TurRec |
+ * | LCTRL  | 2GUI   | TurTog | LALT   | SPACE           | LEFT   | DOWN   | RIGHT  | F5 quicksave    | TurSpd | Exit            | TurRec |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
   [_G1] = LAYOUT_ortho_5x15(
     KC_GRV,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
-    KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    DM_PLY1, XXXXXXX, KC_VOLU, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-    KC_ESC,  KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    DM_PLY2, XXXXXXX, KC_MUTE, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    XXXXXXX, KC_UP,   KC_VOLD, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    KC_LCTL, TD(TD2G), KC_TURT, KC_LALT, KC_SPC,  XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_SPC,  XXXXXXX, KC_TURS, TG(_G1), XXXXXXX, KC_TURB
+    KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_F1,   KC_F2,   KC_VOLU, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+    KC_ESC,  KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_F3,   KC_F4,   KC_VOLD, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    DM_PLY1, KC_UP,   KC_MUTE, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+    KC_LCTL, TD(TD2G), KC_TURT, KC_LALT, KC_SPC,  XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_F5,   XXXXXXX, KC_TURS, TG(_G1), XXXXXXX, KC_TURB
   ),
 
 /* JLAYER
